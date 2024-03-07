85c0d33b59e99b0e558ec0291b1dab8f9197cb09
Sanatan <sanatan.iit@gmail.com>
2024-03-07 13:42:01 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [sitemap(), tailwind()],
   output: "server",
   adapter: vercel(),
   server: (command) => ({ port: command === 'dev' ? 4231 : 4000, host: true })
